9dfbdbea0860d2eaed2b62f08053b102161048a0
Qisheng Jiang <qisheng.jiang@duke.edu>
2025-12-24 02:29:17 +0000

    test run

--- src/run.sh
+++ src/run.sh
@@ -1,37 +1,20 @@
 #!/bin/bash
 
-# run . ./setup.sh to set up the environment variables first 
+set -e
+set -x
 
-docker_name=constantine
-
-docker exec $docker_name bash -c \
-"cd /root/constantine/src && \
-. ./setup.sh && \
-rm -f ./bin/dfl.bcc && rm -f ./bin/cfl.bcc && \
-cd lib && rm -f ./dfl/dfl.o && make install -j10 && \
-cd /root/constantine/src && \
-cd passes && rm -f ./dfl/dfl.so ./dfl/dfl.o && make install -j10" 
+ROOT=/app/src
 
 FILE=$1
 FILE_NAME=${FILE::-2}
 
-docker exec $docker_name bash -c \
-"cd /root/constantine/src && \
-. ./setup.sh && \
-rm -f $FILE_NAME.out $FILE_NAME.base.bc $FILE_NAME.dfl.bc $FILE_NAME.final.bc && \
-rm -f $FILE_NAME.base.ll $FILE_NAME.dfl.ll $FILE_NAME.final.ll && \
-rm -f $FILE_NAME.final.s $FILE_NAME.final.o && \
-./constantine -O0 $FILE -o $FILE_NAME.out && \
-llvm-dis $FILE_NAME.base.bc -o $FILE_NAME.base.ll && \
-llvm-dis $FILE_NAME.dfl.bc -o $FILE_NAME.dfl.ll && \
-llvm-dis $FILE_NAME.final.bc -o $FILE_NAME.final.ll "
+cd $ROOT
+. ./setup.sh
+./constantine -O0 $FILE -o $FILE_NAME.out || true 
+llvm-dis $FILE_NAME.final.bc -o $FILE_NAME.final.ll
 
+llc-13 -march=x86-64 -mattr=+avx512f,+avx512vl $FILE_NAME.final.bc
+clang-13 -c $FILE_NAME.final.s -o $FILE_NAME.final.o
+clang-13 -no-pie -o $FILE_NAME.out $FILE_NAME.final.o
 
-llc -march=x86-64 -mattr=+avx512f,+avx512vl $FILE_NAME.final.bc
-clang -c $FILE_NAME.final.s -o $FILE_NAME.final.o
-clang -no-pie -o $FILE_NAME.out $FILE_NAME.final.o
-
-# -dfl-avx512=1 -dfl-avx2=1 in constantine 
-# #define DFL_STRIDE (64uL) in dfl.c 
-
-# ../retdec/bin/retdec-decompiler $FILE_NAME.out > /dev/null
+echo "Success: $FILE_NAME.out"
